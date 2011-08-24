593151e808b13d44694c6af2de40cf31274aa6e6
Ian Li <ianarli@gmail.com>
2011-08-24 18:40:43 -0400

    Added config/ and db/ while ignoring some files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hciibb::Application.config.secret_token = 'd72c36a6d3eb4c9a45b66937b1c80be9f762c7032c87a8f81fbe30bfcae832105ac970f4a4400257d8b724cfa5c3f59d67804bec3a759c209b127063414afa93'
---
+++ db/migrate/20110824210011_create_people.rb
@@ -0,0 +1,17 @@
+class CreatePeople < ActiveRecord::Migration
+  def self.up
+    create_table :people do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :room
+      
+      t.timestamps
+    end
+    
+    add_index :people, [:last_name, :first_name], :unique => true
+  end
+
+  def self.down
+    drop_table :people
+  end
+end
